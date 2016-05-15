e69df3cb7e02a002bd9e0ee65a14d50c64215278
sami fattah <oasf2016@gmail.com>
2016-05-15 03:02:30 -0400

    test

--- app/src/main/java/com/samifattah/brooks21/util/Utility.java
+++ app/src/main/java/com/samifattah/brooks21/util/Utility.java
@@ -3,24 +3,39 @@ package com.samifattah.brooks21.util;
 import android.os.Environment;
 import android.util.Log;
 
-
 public class Utility
 {
 	private static final String TAG = "Utility";
 	
-	public static boolean s_bIsLogEnabloed = true;
+	public static boolean s_bIsDebugEnabloed = true;
 	
 
 	public static void Assert()
 	{
-		throw new RuntimeException( "Assert" );
+        if(s_bIsDebugEnabloed)
+        {
+            throw new RuntimeException( "Assert" );
+        }
 	}
+
+    public static void Assert( boolean bTest )
+    {
+        if(s_bIsDebugEnabloed)
+        {
+
+            if (bTest == false) {
+                throw new RuntimeException("IQAssert");
+            }
+        }
+    }
 	
 	public static void Assert( String szMessage )
 	{
-		throw new RuntimeException( szMessage );
+        if(s_bIsDebugEnabloed)
+        {
+            throw new RuntimeException(szMessage);
+        }
 	}
-	
 
 	public static String getPathToSDCard()
 	{
@@ -29,7 +44,7 @@ public class Utility
 	
 	public static void logDebug( String szTage , String szMessage )
 	{
-		if( s_bIsLogEnabloed)
+		if( s_bIsDebugEnabloed )
 		{
 			Log.d(szTage, szMessage);
 		}
