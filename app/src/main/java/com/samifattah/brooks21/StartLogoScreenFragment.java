d296acbed039af9efaf41ba35d0efe56c32e32a7
sami fattah <oasf2016@gmail.com>
2016-05-15 01:19:23 -0400

    Rejected commit: Default

--- app/src/main/java/com/samifattah/brooks21/StartLogoScreenFragment.java
+++ app/src/main/java/com/samifattah/brooks21/StartLogoScreenFragment.java
@@ -21,7 +21,7 @@ public class StartLogoScreenFragment extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.fragment_startlogoscreen1, container, false);
+        return inflater.inflate(R.layout.fragment_startlogoscreen, container, false);
     }
 
 
