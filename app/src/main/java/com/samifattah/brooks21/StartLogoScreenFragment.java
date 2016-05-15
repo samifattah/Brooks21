5ae70b325e287f46175726a7ae334b4bae40ff97
sami fattah <oasf2016@gmail.com>
2016-05-15 01:17:37 -0400

    Rejected commit: Default

--- app/src/main/java/com/samifattah/brooks21/StartLogoScreenFragment.java
+++ app/src/main/java/com/samifattah/brooks21/StartLogoScreenFragment.java
@@ -1,7 +1,6 @@
 package com.samifattah.brooks21;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -22,7 +21,7 @@ public class StartLogoScreenFragment extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.fragment_startlogoscreen, container, false);
+        return inflater.inflate(R.layout.fragment_startlogoscreen1, container, false);
     }
 
 
