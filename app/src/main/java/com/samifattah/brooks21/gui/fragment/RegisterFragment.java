09296278ccd3e33509e49dc1968a4bd82224810e
osamaabdelfattah <oasf2015@gmail.com>
2016-05-15 16:46:55 -0400

    test

---
+++ app/src/main/java/com/samifattah/brooks21/gui/fragment/RegisterFragment.java
@@ -0,0 +1,82 @@
+package com.samifattah.brooks21.gui.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.samifattah.brooks21.R;
+import com.samifattah.brooks21.util.BaseFragment;
+import com.samifattah.brooks21.util.Utility;
+
+public class RegisterFragment extends BaseFragment
+{
+    private Button m_LoginButton   = null;
+
+    private Button m_LoginRegister = null;
+
+
+    public interface StartLogoScreenFragmentInterface extends BaseFragmentInterface
+    {
+        void whatthefuck();
+    }
+
+    public RegisterFragment()
+    {
+        Utility.logDebug(m_szTag,"StartLogoScreenFragment");
+
+        m_szTag = new String("StartLogoScreenFragment");
+
+        m_iLayoutID = R.layout.fragment_register;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        Utility.logDebug(m_szTag,"onCreateView");
+
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        m_LoginButton   = (Button) m_View.findViewById(R.id.loginbutton);
+
+        m_LoginRegister = (Button) m_View.findViewById(R.id.registerbutton);
+
+        Utility.Assert(m_LoginButton!=null);
+
+        Utility.Assert(m_LoginRegister!=null);
+
+        if(m_LoginButton!=null)
+        {
+            m_LoginButton.setOnClickListener(this);
+        }
+
+
+        if(m_LoginRegister!=null)
+        {
+            m_LoginRegister.setOnClickListener(this);
+        }
+
+        return m_View;
+    }
+
+    @Override
+    public void handleClick(View v)
+    {
+
+        Utility.logDebug(m_szTag,"handleClick");
+
+        if(v.getId()==this.m_LoginButton.getId())
+        {
+            StartLogoScreenFragmentInterface startLogoScreenFragmentInterface = (StartLogoScreenFragmentInterface) m_BaseFragmentInterface;
+
+            startLogoScreenFragmentInterface.whatthefuck();
+        }
+
+        if(v.getId()==this.m_LoginRegister.getId())
+        {
+
+        }
+
+    }
+}
